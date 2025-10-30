311ce1087e7e7e9b51ce5147090707e8ffd70677
princek <princesamuelpks@gmail>
2025-10-30 19:25:04 +0000

    Update serializers.py

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -3,9 +3,6 @@ from rest_framework import serializers
 
 from accounts.models import User
 
-from accounts.models import Location
-
-
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
