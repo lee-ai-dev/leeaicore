d9e66cf5cb258fb63e02e12a9414e7fd99b08e2f
princek <princesamuelpks@gmail>
2025-12-24 09:58:37 +0000

    Update urls.py

--- leeaicore/urls.py
+++ leeaicore/urls.py
@@ -7,6 +7,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-v1/', include('api.urls')),
+    path('api-v1/integrations/', include('integrations.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
 ]
